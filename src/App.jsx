8d554197cf05367db61479913c1f1518db9e6973
yujin <kimyujin.ked@baenergy.net>
2025-06-09 11:38:39 +0900

    3. 로그인 버튼을 누르면 로그인 페이지가 나온다

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Navbar from "./component/Navbar";
 
 // 1. 전체상품페이지, 로그인, 상품상세페이지 ✅
 // 2. 전체 상품페이지에서는 전체 상품을 볼 수 있다 ✅
-// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다 ✅
 // 3. 상품디테일을 눌렀으나, 로그인이 안되있을 경우에는 로그인페이지가 먼저 나온다
 // 4. 로그인이 되어있을 경우에는 상품 디테일 페이지를 볼 수 있다
 // 5. 로그아웃 버튼을 클릭하면 로그아웃이 된다
@@ -17,6 +17,8 @@ import Navbar from "./component/Navbar";
 // 6. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다
 // 7. 상품을 검색할 수 있다
 function App() {
+  const [authenticate, setAuthenticate] = useState(false); // true면 로그인이 됨, false면 로그인이 안됨
+
   return (
     <div>
       <Navbar />
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const menuList = [
@@ -15,10 +16,16 @@ const Navbar = () => {
     "지속가능성",
   ];
 
+  const navigate = useNavigate();
+
+  const goToLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <div>
       <div>
-        <div className="login-button">
+        <div className="login-button" onClick={goToLogin}>
           <FontAwesomeIcon icon={faUser} />
           <div>로그인</div>
         </div>
@@ -32,7 +39,7 @@ const Navbar = () => {
       <div className="menu-area">
         <ul className="menu-list">
           {menuList.map((menu) => (
-            <li>{menu}</li>
+            <li key={menu}>{menu}</li>
           ))}
         </ul>
         <div className="search-box">
